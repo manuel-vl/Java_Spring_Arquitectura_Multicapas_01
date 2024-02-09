7328505f10134172338e8a4ebb5da3a47e95268f
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-09 10:07:43 -0500

    [FEATURE] Add persona class and json file

---
+++ src/main/java/com/exercisearquitecturamulticapas/arquitecturamulticapas/model/Personaje.java
@@ -0,0 +1,19 @@
+package com.exercisearquitecturamulticapas.arquitecturamulticapas.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Personaje {
+    private String name;
+    private Double height;
+    private Double mass;
+    private String hairColor;
+    private String skinColor;
+    private String eyeColor;
+    private String birthYear;
+    private String gender;
+    private String homeworld;
+    private String species;
+}
