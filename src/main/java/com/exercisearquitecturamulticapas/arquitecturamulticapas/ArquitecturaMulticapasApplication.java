5099ecbe61c744f215f53b8ac28c15d2ae50ef2a
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-09 09:35:35 -0500

    Initial commit

---
+++ src/main/java/com/exercisearquitecturamulticapas/arquitecturamulticapas/ArquitecturaMulticapasApplication.java
@@ -0,0 +1,13 @@
+package com.exercisearquitecturamulticapas.arquitecturamulticapas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquitecturaMulticapasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquitecturaMulticapasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercisearquitecturamulticapas/arquitecturamulticapas/ArquitecturaMulticapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercisearquitecturamulticapas.arquitecturamulticapas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquitecturaMulticapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
